8e759237935676dc7ed7c5eec9f6cf5f66cb6c1d
ShemyakinAV <antonshemyakin97@gmail.com>
2020-02-06 16:49:52 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, {Component} from 'react'
+import React, {Component,useState, useEffect } from 'react'
 import Layout from './hoc/Layout/Layout'
 import {Route, Switch} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
 import QuizList from './containers/QuizList/QuizList'
 import Auth from './containers/Auth/Auth'
 import QuizCreator from './containers/QuizCreator/QuizCreator'
-import React, { useState, useEffect } from 'react';
 import connect from '@vkontakte/vk-connect';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
 
 class App extends Component {
   render() {
