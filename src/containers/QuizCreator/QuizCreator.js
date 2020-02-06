b2a857226ed7251343d7e6d2afcc4826ff8f7005
ShemyakinAV <antonshemyakin97@gmail.com>
2020-02-06 14:19:12 +0300

    6 02 office

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -2,103 +2,177 @@ import React, {Component} from 'react'
 import classes from './QuizCreator.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
-import {createControl} from '../../form/formFramework'
+import Select from '../../components/UI/Select/Select'
+import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import axios from "../../axios/axios";
 
 function createOptionControl(number) {
-  return createControl({
-    label: `Вариант ${number}`,
-    errorMessage: 'Значение не может быть пустым',
-    id: number
-  }, {required: true})
+    return createControl({
+        label: `Вариант ${number}`,
+        errorMessage: 'Значение не может быть пустым',
+        id: number
+    }, {required: true})
 }
 
 function createFormControls() {
-  return {
-    question: createControl({
-      label: 'Введите вопрос',
-      errorMessage: 'Вопрос не может быть пустым'
-    }, {required: true}),
-    option1: createOptionControl(1),
-    option2: createOptionControl(2),
-    option3: createOptionControl(3),
-    option4: createOptionControl(4)
-  }
+    return {
+        question: createControl({
+            label: 'Введите вопрос',
+            errorMessage: 'Вопрос не может быть пустым'
+        }, {required: true}),
+        option1: createOptionControl(1),
+        option2: createOptionControl(2),
+        option3: createOptionControl(3),
+        option4: createOptionControl(4)
+    }
 }
 
 export default class QuizCreator extends Component {
 
-  state = {
-    quiz: [],
-    formControls: createFormControls()
-  }
+    state = {
+        quiz: [],
+        isFormValid: false,
+        rightAnswerId: 1,
+        formControls: createFormControls()
+    }
 
-  sibmitHandler = event => {
-    event.preventDefault()
-  }
+    sibmitHandler = event => {
+        event.preventDefault()
+    }
 
-  addQuestionHandler = () => {
+    addQuestionHandler = event => {
+        event.preventDefault()
 
-  }
+        const quiz = this.state.quiz.concat()
+        const index = quiz.length + 1
 
-  createQuizHandler = () => {
+        const {question, option1, option2, option3, option4} = this.state.formControls
 
-  }
+        const questionItem = {
+            question: question.value,
+            id: index,
+            rightAnswerId: this.state.rightAnswerId,
+            answers: [
+                {text: option1.value, id: option1.id},
+                {text: option2.value, id: option2.id},
+                {text: option3.value, id: option3.id},
+                {text: option4.value, id: option4.id}
+            ]
+        }
 
-  changeHandler = (value, controlName) => {
+        quiz.push(questionItem)
 
-  }
+        this.setState({
+            quiz,
+            isFormValid: false,
+            rightAnswerId: 1,
+            formControls: createFormControls()
+        })
+    }
 
-  renderControls() {
-    return Object.keys(this.state.formControls).map((controlName, index) => {
-      const control = this.state.formControls[controlName]
+    createQuizHandler = async event => {
+        event.preventDefault()
+        try {
+            await axios.post('quizes.json', this.state.quiz)
+            this.setState({
+                quiz: [],
+                isFormValid: false,
+                rightAnswerId: 1,
+                formControls: createFormControls()
 
-      return (
-        <Auxiliary key={controlName + index}>
-          <Input
-            label={control.label}
-            value={control.value}
-            valid={control.valid}
-            shouldValidate={!!control.validation}
-            touched={control.touched}
-            errorMessage={control.errorMessage}
-            onChange={event => this.changeHandler(event.target.value, controlName)}
-          />
-          { index === 0 ? <hr /> : null }
-        </Auxiliary>
-      )
-    })
-  }
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
-  render() {
-    return (
-      <div className={classes.QuizCreator}>
-        <div>
-          <h1>Создание теста</h1>
+    changeHandler = (value, controlName) => {
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[controlName]}
 
-          <form onSubmit={this.submitHandler}>
+        control.touched = true
+        control.value = value
+        control.valid = validate(control.value, control.validation)
 
-            { this.renderControls() }
+        formControls[controlName] = control
 
-            <select></select>
+        this.setState({
+            formControls,
+            isFormValid: validateForm(formControls)
+        })
+    }
 
-            <Button
-              type="primary"
-              onClick={this.addQuestionHandler}
-            >
-              Добавить вопрос
-            </Button>
+    renderControls() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
 
-            <Button
-              type="success"
-              onClick={this.createQuizHandler}
-            >
-              Создать тест
-            </Button>
+            return (
+                <Auxiliary key={controlName + index}>
+                    <Input
+                        label={control.label}
+                        value={control.value}
+                        valid={control.valid}
+                        shouldValidate={!!control.validation}
+                        touched={control.touched}
+                        errorMessage={control.errorMessage}
+                        onChange={event => this.changeHandler(event.target.value, controlName)}
+                    />
+                    {index === 0 ? <hr/> : null}
+                </Auxiliary>
+            )
+        })
+    }
 
-          </form>
-        </div>
-      </div>
-    )
-  }
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        })
+    }
+
+    render() {
+        const select = <Select
+            label="Выберите правильный ответ"
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4}
+            ]}
+        />
+
+        return (
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+
+                    <form onSubmit={this.submitHandler}>
+
+                        {this.renderControls()}
+
+                        {select}
+
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Добавить вопрос
+                        </Button>
+
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}
+                            disabled={this.state.quiz.length === 0}
+                        >
+                            Создать тест
+                        </Button>
+
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
 import App from './App'
+import {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 const app = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
